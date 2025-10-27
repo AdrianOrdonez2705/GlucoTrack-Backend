0632c61e3827e43c8b781217e01bb786d04ea68d
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2025-10-27 16:05:34 -0400

    Testing endpoint

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // ENDPOINTS 
+
+// Endpoint the prueba GET de todos los usuarios
+app.get('/usuarios', async (req, res) => {
+    const { data, error } = await supabase.from('usuario').select('*');
+
+    if (error) return res.status(500).json({ error: error.message });
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
